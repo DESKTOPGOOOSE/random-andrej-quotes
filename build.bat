2d66b4d82120581e4b13f8ffc61094def4c81c02
Longhorn5048 <77574002+DESKTOPGOOOSE@users.noreply.github.com>
2022-01-12 23:11:45 -0500

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Compile code
+cd src/main/java
+javac AndrejQuote.java
+echo echo Main-Class: AndrejQuote >manifest.txt
+echo Build jar
+mkdir bin
+jar cvfm AndrejQuote.jar manifest.txt *.class
+move AndrejQuote.jar bin
+echo Jar was built at %cd%/src/main/java/bin/AndrejQuote.jar
