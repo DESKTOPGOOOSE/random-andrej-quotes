1ce2b5ff56bf75f38380f321dd9d8ed32e1c43a8
Longhorn5048 <77574002+DESKTOPGOOOSE@users.noreply.github.com>
2022-01-12 22:28:28 -0500

    Update AndrejQuote.java

--- src/main/java/AndrejQuote.java
+++ src/main/java/AndrejQuote.java
@@ -18,32 +18,32 @@ import java.util.List;
 
 public class AndrejQuote {
     public static void main(String[] args) throws Exception {
-        String cd = System.getProperty("user.dir");
-        try (BufferedInputStream inputStream = new BufferedInputStream(new URL("https://raw.githubusercontent.com/DESKTOPGOOOSE/random-andrej-quotes/main/quotes.txt").openStream());
+        String cd = System.getProperty("user.dir"); // First thing's first, we should get the current directory prevent explosion later.
+        try (BufferedInputStream inputStream = new BufferedInputStream(new URL("https://raw.githubusercontent.com/DESKTOPGOOOSE/random-andrej-quotes/main/quotes.txt").openStream()); // Let's download all the 1,233 quotes!
              FileOutputStream fileOS = new FileOutputStream(cd + "/quotes.txt")) {
             byte data[] = new byte[1024];
             int byteContent;
             while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
                 fileOS.write(data, 0, byteContent);
             }
-        } catch (IOException e) {
-            System.out.println("Java go boom boom");
+        } catch (IOException e) { // Something happened (Random Andrej Quotes edition)
+            System.out.println("Java go boom boom"); // If they're seeing this, their internet connection blows to the point where they can't even download 39.9 KB files.
         }
 
         // The main fucking thing of this stupid ass program
-        BufferedReader reader = new BufferedReader(new FileReader(cd + "/quotes.txt"));
-        List<String> lines = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new FileReader(cd + "/quotes.txt")); // Let's create new readers to read this damn file.
+        List<String> lines = new ArrayList<String>(); // Let's create a new fucking array list.
 
         String line = reader.readLine();
 
         while ( line != null ) {
             lines.add(line);
-            line = reader.readLine();
+            line = reader.readLine(); // Read line
         }
 
         // Lets choose a random quote now!
-        Random r = new Random();
-        String randomQuote = lines.get(r.nextInt(lines.size()));
-        System.out.println("Your Andrej Akan quote is: " + randomQuote);
+        Random r = new Random(); // Makes your computer do stuff
+        String randomQuote = lines.get(r.nextInt(lines.size())); // Makes your computer do more stuff
+        System.out.println("Your Andrej Akan quote is: " + randomQuote); // Display it to the user.
     }
 }
